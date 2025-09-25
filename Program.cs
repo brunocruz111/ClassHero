4823acadc02beb06d2f7badce9f77003e0e8818a
brunocruz111 <brunocruz@alunos.utfpr.edu.br>
2025-09-24 21:32:44 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,17 +4,25 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Digite sua idade: ");
-        int idadeCliente = int.Parse(Console.ReadLine());
+        int pares = 0;
+        int impares = 0;
 
-        if (idadeCliente >= 18)
+        for (int i = 1; i <= 8; i++)
         {
-            Console.WriteLine("Você é maior de idade.");
+            Console.Write($"Digite o {i}º número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                pares++;
+            }
+            else
+            {
+                impares++;
+            }
         }
-        else
-        {
-            Console.WriteLine("Você é menor de idade.");
-        }
+
+        Console.WriteLine($"\nQuantidade de números pares: {pares}");
+        Console.WriteLine($"Quantidade de números ímpares: {impares}");
     }
 }
-
