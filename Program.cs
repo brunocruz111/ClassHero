42a7b141714345ec169b12613908d70f4b0fd346
brunocruz111 <brunocruz@alunos.utfpr.edu.br>
2025-09-24 21:26:55 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
-string nomeUsuarioLogin;
-int idadeCliente;
-decimal valorTotalPedido;
-bool cadastroAtivo;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite sua idade: ");
+        int idadeCliente = int.Parse(Console.ReadLine());
+
+        if (idadeCliente >= 18)
+        {
+            Console.WriteLine("Você é maior de idade.");
+        }
+        else
+        {
+            Console.WriteLine("Você é menor de idade.");
+        }
+    }
+}
+
